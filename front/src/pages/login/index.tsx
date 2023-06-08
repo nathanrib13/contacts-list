ec525a8853ec4e616f8e393a84c88740aa676ba1
Nathan Ribeiro <nathan.ribeiros7@gmail.com>
2023-06-08 16:35:55 -0300

    feature: pages login and dashboard created

--- front/src/pages/login/index.tsx
+++ front/src/pages/login/index.tsx
@@ -2,6 +2,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import schema, { loginData } from "./validator";
 import { useAuth } from "../../hooks/useAuth";
+import Container from "./style";
 
 const Login = () => {
   const { register, handleSubmit } = useForm<loginData>({
@@ -10,7 +11,7 @@ const Login = () => {
 
   const { signIn } = useAuth();
   return (
-    <main>
+    <Container>
       <h1>Login</h1>
       <form onSubmit={handleSubmit(signIn)}>
         <label htmlFor="">Email</label>
@@ -20,7 +21,7 @@ const Login = () => {
 
         <button type="submit">Entrar</button>
       </form>
-    </main>
+    </Container>
   );
 };
 
